672c5bff59d8ee31c724a772951aa103d97d8861
AlexMoreni <alex.moreni47@gmail.com>
2023-09-19 15:40:09 -0300

    Creation of the registration and authentication page

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,3 +1,7 @@
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/User");
+
 module.exports = class AuthController {
   static async login(req, res) {
     res.render("auth/login");
@@ -5,5 +9,47 @@ module.exports = class AuthController {
 
   static async register(req, res) {
     res.render("auth/register");
+  }
+
+  static async registerPost(req, res) {
+    const { name, email, password, confirmPassword } = req.body;
+
+    if (password !== confirmPassword) {
+      req.flash("message", "As senhas não conferem, verifique e corrija");
+      res.render("auth/register");
+
+      return;
+    }
+
+    const checkIfUserExist = await User.findOne({ where: { email: email } });
+
+    if (checkIfUserExist) {
+      req.flash("message", "O email já está em umso!");
+      res.render("auth/register");
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    const user = {
+      name,
+      email: email,
+      password: hashedPassword,
+    };
+
+    try {
+      const cratedUser = await User.create(user);
+
+      //initialize session
+      req.session.userid = cratedUser.id;
+
+      req.flash("message", "Cadastro realizado com sucesso!");
+
+      req.session.save(() => {
+        res.redirect("/");
+      });
+    } catch (err) {
+      console.log(err);
+    }
   }
 };
