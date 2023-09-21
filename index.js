4109c8461727ed47c4ebd1ffc90b5da36c6e3d76
AlexMoreni <alex.moreni47@gmail.com>
2023-09-21 11:26:35 -0300

    Settings configuration deployment

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.use(
 app.use(flash());
 
 //Public path
-app.use(express.static("public"));
+app.use(express.static(__dirname + "./" + "/public"));
 
 //Set session to res
 app.use((req, res, next) => {
